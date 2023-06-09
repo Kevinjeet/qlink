0078fec8e9d8732b41794100c2b0fd643c399b4d
Avisha <kumaravisha96@gmail.com>
2023-06-09 13:13:43 -0700

    added corshost2

--- qlink-service/main.py
+++ qlink-service/main.py
@@ -11,7 +11,11 @@ app.include_router(authenticator.router)
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000", os.environ.get("CORS_HOST", None)],
+    allow_origins=[
+        "http://localhost:3000",
+        os.environ.get("CORS_HOST", None),
+        os.environ.get("CORS_HOST2", None),
+    ],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
