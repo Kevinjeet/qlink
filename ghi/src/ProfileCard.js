18a5ff1df08a02da9b38f862c921fd9bedd15030
Kevinjeet <kgill2992@gmail.com>
2023-06-15 00:11:19 +0000

    Checkboxes

--- ghi/src/ProfileCard.js
+++ ghi/src/ProfileCard.js
@@ -146,115 +146,116 @@ function ProfileCard({ user, refreshUserInfo }) {
 
 
 
-          <div className="container py-5 mx-auto border border-gray-500 overflow-hidden">
+          <div className="container py-5 mx-auto bg-blue-200 border border-gray-500 overflow-hidden">
           <h1 className=" text-center mb-4 font-bold text-gray-900 dark:text-white">Check tags</h1>
-          <h2 className="text-center mb-2 text-gray-900 dark:text-white">Gender</h2>
+          <h2 className="text-center mb-2 bg-purple-100 text-gray-900 dark:text-white">Gender</h2>
 <ul className="text-sm font-medium text-gray-900 bg-white border border-gray-200 rounded-lg dark:bg-gray-700 dark:border-gray-600 dark:text-white inline-flex flex-wrap">
     <li className="rounded-t-sm dark:border-gray-600 inline-flex flex-wrap sm:block">
         <div className="flex items-center pl-3">
-            <input id="vue-checkbox"
+            <input id="vue-checkbox-male"
             type="checkbox"
             value="male"
             className="h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600
             dark:ring-offset-gray-700 dark:focus:ring-offset-gray-700 focus:ring-2 dark:bg-gray-600 dark:border-gray-500"
             onChange={handleCheckbox}/>
-            <label for="vue-checkbox"
+            <label
+            htmlFor="vue-checkbox-male"
             className="w-full py-3 ml-2 text-sm font-medium text-gray-900 dark:text-gray-300">
-              <div className="bg-gray-200 px-2 py-1 rounded-lg">male</div></label>
+              <div className="bg-orange-200 px-2 py-1 rounded-lg">male</div></label>
         </div>
     </li>
     <li className="rounded-t-sm dark:border-gray-600 inline-flex flex-wrap sm:block">
         <div className="flex items-center pl-3">
-            <input id="vue-checkbox"
+            <input id="vue-checkbox-female"
             type="checkbox"
             value="female"
             className="h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600
             dark:ring-offset-gray-700 dark:focus:ring-offset-gray-700 focus:ring-2 dark:bg-gray-600 dark:border-gray-500"
             onChange={handleCheckbox}/>
-            <label for="vue-checkbox"
+            <label htmlFor="vue-checkbox-female"
             className="w-full py-3 ml-2 text-sm font-medium text-gray-900 dark:text-gray-300">
-              <div className="bg-gray-200 px-2 py-1 rounded-lg">female</div></label>
+              <div className="bg-orange-200 px-2 py-1 rounded-lg">female</div></label>
         </div>
     </li>
     <li className="rounded-t-sm dark:border-gray-600 inline-flex flex-wrap sm:block">
         <div className="flex items-center pl-3">
-            <input id="vue-checkbox"
+            <input id="vue-checkbox-Non-binary"
             type="checkbox"
             value="Non-binary"
             className="h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600
             dark:ring-offset-gray-700 dark:focus:ring-offset-gray-700 focus:ring-2 dark:bg-gray-600 dark:border-gray-500"
             onChange={handleCheckbox}/>
-            <label for="vue-checkbox"
+            <label htmlFor="vue-checkbox-Non-binary"
             className="w-full py-3 ml-2 text-sm font-medium text-gray-900 dark:text-gray-300">
-              <div className="bg-gray-200 px-2 py-1 rounded-lg">Non-binary</div></label>
+              <div className="bg-orange-200 px-2 py-1 rounded-lg">Non-binary</div></label>
         </div>
     </li>
     <li className="rounded-t-sm dark:border-gray-600 inline-flex flex-wrap sm:block">
         <div className="flex items-center pl-3">
-            <input id="vue-checkbox"
+            <input id="vue-checkbox-Gender-nonconforming"
             type="checkbox"
             value="Gender nonconforming"
             className="h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600
             dark:ring-offset-gray-700 dark:focus:ring-offset-gray-700 focus:ring-2 dark:bg-gray-600 dark:border-gray-500"
             onChange={handleCheckbox}/>
-            <label for="vue-checkbox"
+            <label htmlFor="vue-checkbox-Gender-nonconforming"
             className="w-full py-3 ml-2 text-sm font-medium text-gray-900 dark:text-gray-300">
-              <div className="bg-gray-200 px-2 py-1 rounded-lg">Gender nonconforming</div></label>
+              <div className="bg-orange-200 px-2 py-1 rounded-lg">Gender nonconforming</div></label>
         </div>
     </li>
 </ul>
-<h2 className="text-center mb-2 text-gray-900 dark:text-white">Interests</h2>
+<h2 className="text-center mb-2 bg-purple-100 text-gray-900 dark:text-white">Interests</h2>
 <ul className="text-sm font-medium text-gray-900 bg-white border border-gray-200 rounded-lg dark:bg-gray-700 dark:border-gray-600 dark:text-white inline-flex flex-wrap">
     <li className="rounded-t-sm dark:border-gray-600 inline-flex flex-wrap sm:block">
         <div className="flex items-center pl-3">
-            <input id="vue-checkbox"
+            <input id="vue-checkbox-sports"
             type="checkbox"
             value="sports"
             className="h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600
             dark:ring-offset-gray-700 dark:focus:ring-offset-gray-700 focus:ring-2 dark:bg-gray-600 dark:border-gray-500"
             onChange={handleCheckbox}/>
-            <label for="vue-checkbox"
+            <label htmlFor="vue-checkbox-sports"
             className="w-full py-3 ml-2 text-sm font-medium text-gray-900 dark:text-gray-300">
-              <div className="bg-gray-200 px-2 py-1 rounded-lg">sports</div></label>
+              <div className="bg-orange-200 px-2 py-1 rounded-lg">sports</div></label>
         </div>
     </li>
     <li className="rounded-t-sm dark:border-gray-600 inline-flex flex-wrap sm:block">
         <div className="flex items-center pl-3">
-            <input id="vue-checkbox"
+            <input id="vue-checkbox-gaming"
             type="checkbox"
             value="gaming"
             className="h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600
             dark:ring-offset-gray-700 dark:focus:ring-offset-gray-700 focus:ring-2 dark:bg-gray-600 dark:border-gray-500"
             onChange={handleCheckbox}/>
-            <label for="vue-checkbox"
+            <label htmlFor="vue-checkbox-gaming"
             className="w-full py-3 ml-2 text-sm font-medium text-gray-900 dark:text-gray-300">
-              <div className="bg-gray-200 px-2 py-1 rounded-lg">gaming</div></label>
+              <div className="bg-orange-200 px-2 py-1 rounded-lg">gaming</div></label>
         </div>
     </li>
     <li className="rounded-t-sm dark:border-gray-600 inline-flex flex-wrap sm:block">
         <div className="flex items-center pl-3">
-            <input id="vue-checkbox"
+            <input id="vue-checkbox-traveling"
             type="checkbox"
             value="traveling"
             className="h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600
             dark:ring-offset-gray-700 dark:focus:ring-offset-gray-700 focus:ring-2 dark:bg-gray-600 dark:border-gray-500"
             onChange={handleCheckbox}/>
-            <label for="vue-checkbox"
+            <label htmlFor="vue-checkbox-traveling"
             className="w-full py-3 ml-2 text-sm font-medium text-gray-900 dark:text-gray-300">
-              <div className="bg-gray-200 px-2 py-1 rounded-lg">traveling</div></label>
+              <div className="bg-orange-200 px-2 py-1 rounded-lg">traveling</div></label>
         </div>
     </li>
     <li className="rounded-t-sm dark:border-gray-600 inline-flex flex-wrap sm:block">
         <div className="flex items-center pl-3">
-            <input id="vue-checkbox"
+            <input id="vue-checkbox-art"
             type="checkbox"
             value="art"
             className="h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600
             dark:ring-offset-gray-700 dark:focus:ring-offset-gray-700 focus:ring-2 dark:bg-gray-600 dark:border-gray-500"
             onChange={handleCheckbox}/>
-            <label for="vue-checkbox"
+            <label htmlFor="vue-checkbox-art"
             className="w-full py-3 ml-2 text-sm font-medium text-gray-900 dark:text-gray-300">
-              <div className="bg-gray-200 px-2 py-1 rounded-lg">art</div></label>
+              <div className="bg-orange-200 px-2 py-1 rounded-lg">art</div></label>
         </div>
     </li>
 </ul></div></div><div className="w-3/4">
@@ -267,7 +268,7 @@ function ProfileCard({ user, refreshUserInfo }) {
                   key={u?.username}
                   value={u?.username}
                 >
-                  <div className="w-full max-w-xs mx-auto my-10 bg-white rounded-sm shadow-md p-5">
+                  <div className="w-full max-w-xs mx-auto my-10 bg-orange-200 rounded-sm shadow-md p-5">
                     {u.profile_picture_url !== "" &&
                     u.profile_picture_url !== "string" ? (
                       <img
@@ -283,31 +284,32 @@ function ProfileCard({ user, refreshUserInfo }) {
                       />
                     )}
                     <h1 className="text-center text-2xl font-bold mt-3">
-                      {u?.username}
+                      <span className="bg-purple-100 rounded-sm shadow-md inline-block">{u?.username}</span>
                     </h1>
                     <h2 className="text-center text-2xl font-semibold mt-3">
-                      {u.first_name} {u.last_name}
+                      <span className="bg-purple-100 rounded-sm shadow-md inline-block">{u.first_name} {u.last_name}</span>
                     </h2>
                     <p className="text-center text-gray-600 mt-1">
-                      {u.location}
+                      <span className="bg-purple-100 rounded-sm shadow-md inline-block">{u.location}</span>
                     </p>
                     <div className="flex justify-center mt-5">
                       <button
                         className="text-blue-500 hover:text-blue-700 mx-3"
                         onClick={() => blockButton(u?.username)}
                       >
-                        Decline
+                        <span className="bg-purple-100 rounded-sm shadow-md inline-block">Decline</span>
                       </button>
                       <button
                         className="text-blue-500 hover:text-blue-700 mx-3"
                         onClick={() => matchButton(u?.username)}
                       >
-                        Match
+                        <span className="bg-purple-100 rounded-sm shadow-md inline-block">Match</span>
                       </button>
                     </div>
                     <div className="mt-5">
                       <h3 className="text-xl font-semibold">Bio</h3>
-                      <p className="text-gray-600 mt-2">{u.about_me}</p>
+                      <p className="text-gray-600 mt-2">
+                        <span className="bg-white rounded-sm shadow-md inline-block">{u.about_me}</span></p>
                     </div>
                     <div className="flex items-right">
                       <button
